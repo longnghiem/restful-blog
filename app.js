9609df534a55849b8785210ef172f66a37224cd4
Long Nghiem <longsuomi@gmail.com>
2018-07-29 00:33:20 +0300

    creating basic template

--- app.js
+++ app.js
@@ -25,6 +25,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/blogs', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+
+const model = require("../model")
+const Blog = model.Blog
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  Blog.find({}, (err, blogs) => {
+    if (err) return handleError(err)
+    else {
+      res.render('index', { blogs });
+    }
+  })
 });
 
 module.exports = router;
