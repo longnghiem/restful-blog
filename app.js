6bd08e44fe10f2f88b4cf8eeee02af5f4168c88c
Long Nghiem <longsuomi@gmail.com>
2018-08-03 01:03:34 +0300

    minor editing

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const methodOverride = require("method-override")
+//use sanitizer in every method that deal with user input
 const expressSanitizer = require("express-sanitizer")
 
 const indexRouter = require('./routes/index');
